8f0240da81f8a4bf4fb90350c2e0f2ca9041788e
PoojaMali18 <141479524+PoojaMali18@users.noreply.github.com>
2024-02-14 13:01:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//your JS code here. If required.
 const output = document.getElementById("output");
 const btn = document.getElementById("download-images-button");
 
@@ -7,3 +6,31 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+btn.addEventListener("click", downloadAndDisplayImages);
+
+function downloadAndDisplayImages() {
+  Promise.all(images.map(downloadImage))
+    .then((results) => {
+      output.innerHTML = ""; // Clear previous content
+      results.forEach((imageElement) => {
+        output.appendChild(imageElement);
+      });
+    })
+    .catch((err) => console.error(err));
+}
+
+function downloadImage(image) {
+  return new Promise((resolve, reject) => {
+    const imgElement = new Image();
+    imgElement.src = image.url;
+
+    imgElement.onload = () => {
+      resolve(imgElement);
+    };
+
+    imgElement.onerror = () => {
+      reject(new Error(`Failed to load image's URL: ${image.url}`));
+    };
+  });
+}
